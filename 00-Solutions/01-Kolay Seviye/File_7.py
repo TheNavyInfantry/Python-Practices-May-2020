88059ff1bb23fe11f6c4fe792ffe138b1c72064d
Nedim Can Ulusoy <nedimcanulusoy@gmail.com>
2020-06-01 18:42:22 +0300

    add solution of file_7

---
+++ 00-Solutions/01-Kolay Seviye/File_7.py
@@ -0,0 +1,12 @@
+def centuryfromyear(year):
+    if len(year) <= 3:
+        converter = year[:1]
+        converted = int(converter) + 1
+        return "{}. Century".format(converted)
+    else:
+        converter = year[:2]
+        converted = int(converter) + 1
+        return "{}. Century".format(converted)
+
+user_input=str(input("Type a year to convert it into the century: "))
+print(centuryfromyear(user_input))
