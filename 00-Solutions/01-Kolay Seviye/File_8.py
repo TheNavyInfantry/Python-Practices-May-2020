52814e3a7aa6a17c343598638e346df6902d7a07
Nedim Can Ulusoy <nedimcanulusoy@gmail.com>
2020-06-06 19:57:12 +0300

    add solution of 01- Kolay Seviye file_8 FIXED

--- 00-Solutions/01-Kolay Seviye/File_8.py
+++ 00-Solutions/01-Kolay Seviye/File_8.py
@@ -1,33 +1,3 @@
-
-limit = int(input('Type a limit: '))
-count = 0
-
-numbers = []
-while count < limit:
-    user_Input = input('Enter your numbers: ')
-    numbers.append(user_Input)
-    count +=1
-    print(numbers)
-
-sum_List = []
-for value in numbers:
-    operation = int(value) * numbers.index(value)
-    sum_List.append(operation)
-print(sum(sum_List))
-
-
-
-
-
-        # print('each', value, 'index', numbers.index(value))
-
-
-
-
-
-
-
-
 def index_multiplier():
     limit = int(input('Type a limit: '))
     count = 0
@@ -37,24 +7,13 @@ def index_multiplier():
         user_Input = input('Enter your numbers: ')
         numbers.append(user_Input)
         count += 1
-        print(numbers)
+        print('Number list is: ',numbers)
 
     sum_List = []
     for value in numbers:
         operation = int(value) * numbers.index(value)
         sum_List.append(operation)
-    print(sum(sum_List))
+    print('Sum is: ',sum(sum_List))
 
-print('Sum is: ', index_multiplier())
-
-
-
-
-
-
-
-
-
-
-
+index_multiplier()
 
