a22bdd5a495319a8c71f0af2833ea2190a881a80
Nedim Can Ulusoy <nedimcanulusoy@gmail.com>
2020-06-15 16:47:36 +0300

    add solution of 01- Kolay Seviye file_20

---
+++ 00-Solutions/01-Kolay Seviye/File_20.py
@@ -0,0 +1,33 @@
+def simplification(data):
+    if type(data) == list:
+        return simplification(data[0])
+    else:
+        return data
+
+data = [[[[[[[[[[[[[[[[['Merhaba']]]]]]]]]]]]]]]]]
+print(simplification(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# found = False
+# for sublist in data:
+#
+#     if search in sublist:
+#         print('Here')
+#         found = True
+#     else:
+#         print('Not here!')
+#         print(data)
